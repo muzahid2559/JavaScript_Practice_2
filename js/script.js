56c523e21f1a9e78c4ac7008926fd070619cd432
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-09 18:08:57 +0600

    JSON Object and Array

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
-// JSON.stringify() --> JS object to JSON String
-// JSON.parse() -->  JSON String to JS Object
-
-var person1 = {
-    "name": "Rahim",
-    "age": 25,
-    "hometown": "Dhaka",
-    "married": false
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var data = this.responseText;
+        //console.log(data);
+        jsonData(data);
+    }
 };
+xmlhttp.open("GET", "data.json", true);
+xmlhttp.send();
 
-var person2 = {
-    name: "Karim",
-    age: 35,
-    hometown: "Chittagong",
-    married: true
-};
 
-console.log(person2);
-console.log(person1);
+
+function jsonData(json_obj) {
+    //console.log(json_obj);
+    var js_obj = JSON.parse(json_obj);
+    //console.log(js_obj);
+
+    for (x in js_obj.persons) {
+        //console.log(x);
+      
+        var persons = js_obj.persons;
+        // console.log(persons[x]);
+        for (y in persons[x]) {
+            console.log(persons[x][y]);
+        }
+        
+    }
+}
