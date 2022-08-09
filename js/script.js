2890e9feabca4290c89a08bcbb751810e27e0249
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-09 17:19:43 +0600

    JSON Data Types

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+//https://jsonlint.com (ai website ta holo: amra je JSON likhsi seta valid kina dekhar jnno)
+
+//string
+//number
+//Object(JSON)
+//array 
+//boolean
+//null
+
+///Doesn't Support undefined,date,function
 var student = {
     name: "Rahim",
     age: 26,
-    hometown: "Dhaka"
+    hometown: "Dhaka",
+    married: false,
+    dob: 1995-05-12,
+    test_null: null,
+    test_undefined: undefined,
+    greet: function(){ //function
+        console.log(`Hello ${this.name}`);
+    }
 };
 
 var student_json = JSON.stringify(student);
