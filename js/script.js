becdb7cbec2b3594e1836983fd9ca7c86b9ca36e
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-09 21:41:03 +0600

    AJAX Intro

--- js/script.js
+++ js/script.js
@@ -1,29 +1,45 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var data = this.responseText;
-        //console.log(data);
-        jsonData(data);
-    }
-};
-xmlhttp.open("GET", "data.json", true);
-xmlhttp.send();
+// Asynchronous Programming
+// AJAX = Asynchronous JavaScript And XML
+// Get data without loading the page
+
+document.getElementById('get_data').addEventListener('click', loadData);
+
+function loadData() {
+    // Create XHR Object (Xml Http Request)
+    let xhr = new XMLHttpRequest();
+
+    // Open
+    xhr.open('GET', 'data.txt', true);
 
 
+   // xhr.onprogress = function() {
+       // console.log(xhr.readyState);
+    //}
 
-function jsonData(json_obj) {
-    //console.log(json_obj);
-    var js_obj = JSON.parse(json_obj);
-    //console.log(js_obj);
-
-    for (x in js_obj.persons) {
-        //console.log(x);
-      
-        var persons = js_obj.persons;
-        // console.log(persons[x]);
-        for (y in persons[x]) {
-            console.log(persons[x][y]);
+    xhr.onload = function () {
+        // HTTP statuses
+        // 200: "OK"
+        // 403: "Forbidden"
+        // 404: "Not Found"
+        if (this.status === 200) {
+            document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4?`
         }
-        
     }
-}
+
+    //xhr.onreadystatechange = function () {
+    //     // readyState Values
+    //     // 0: request not initialized
+    //     // 1: server connection established
+    //     // 2: request received
+    //     // 3: processing request
+    //     // 4: request finished and response is ready
+    //     //console.log(this.readyState);
+    //     if (this.status === 200 && this.readyState === 4) {
+    //         console.log(this.responseText);
+    //     }
+    // }
+
+    xhr.send();
+
+    //console.log(xhr);
+} 
