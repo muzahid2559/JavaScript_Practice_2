51f199190543653ed9297cfc802d325a51728f83
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-09 22:06:48 +0600

    Data From External Server

--- js/script.js
+++ js/script.js
@@ -1,45 +1,37 @@
-// Asynchronous Programming
-// AJAX = Asynchronous JavaScript And XML
-// Get data without loading the page
+// http://www.icndb.com/api/
+// API
+// RESTful API
 
-document.getElementById('get_data').addEventListener('click', loadData);
+document.getElementById('get_data').addEventListener('click', loadJokes);
 
-function loadData() {
-    // Create XHR Object (Xml Http Request)
+function loadJokes(e) {
+    let number = document.getElementById('numberJokes').value;
+    //console.log(number);
     let xhr = new XMLHttpRequest();
 
-    // Open
-    xhr.open('GET', 'data.txt', true);
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
-
-   // xhr.onprogress = function() {
-       // console.log(xhr.readyState);
-    //}
+    xhr.onprogress = function () {
+        document.getElementById('output').innerHTML = "<h3>Loading......</h3>";
+    }
 
     xhr.onload = function () {
-        // HTTP statuses
-        // 200: "OK"
-        // 403: "Forbidden"
-        // 404: "Not Found"
         if (this.status === 200) {
-            document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4?`
+            let data = JSON.parse(this.responseText);
+            let jokes = data.value;
+            let output = "<ol>";
+
+            jokes.forEach(function (item) {
+                //console.log(item.joke);
+                output += `<li>${item.joke}</li>`;
+            });
+            output += "</ol>";
+
+            document.getElementById('output').innerHTML = output;
+            //console.log(jokes);
+            //console.log(data);
         }
     }
 
-    //xhr.onreadystatechange = function () {
-    //     // readyState Values
-    //     // 0: request not initialized
-    //     // 1: server connection established
-    //     // 2: request received
-    //     // 3: processing request
-    //     // 4: request finished and response is ready
-    //     //console.log(this.readyState);
-    //     if (this.status === 200 && this.readyState === 4) {
-    //         console.log(this.responseText);
-    //     }
-    // }
-
     xhr.send();
-
-    //console.log(xhr);
-} 
+}
