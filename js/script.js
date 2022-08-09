408b4a123d23554d24a4586e745f9f8dc9ea5921
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-10 00:26:11 +0600

    JavaScript Promises

--- js/script.js
+++ js/script.js
@@ -1,51 +1,38 @@
-// Callback Function 
-
-// setTimeout(function(){
-//     console.log("Hello World!");
-// }, 5000);
-
-// let persons = [
-//     {firstName: "Muzahidul", lastName: "Islam"},
-//     {firstName: "Fazle", lastName: "Rahat"}
-// ]
-
-// function createPerson(person) {
-//    setTimeout(function() {
-//        persons.push(person);
-//    }, 4000); 
-// }
-
-// function getPerson() {
-//     setTimeout(function(){
-//         let output = '';
-//         persons.forEach(function(person){
-//             output += `<li>${person.firstName} ${person.lastName}</li>`
-//         }); 
-//         document.getElementById('output').innerHTML = output;
-//     }, 1000);
-// }
-
+// Promises
+// .then
 let persons = [
-    {firstName: "Muzahidul", lastName: "Islam"},
-    {firstName: "Fazle", lastName: "Rahat"}
+    { firstName: "Simanta", lastName: "Paul" },
+    { firstName: "Fazle", lastName: "Rahat" }
 ]
 
-function createPerson(person, callback) {
-   setTimeout(function() {
-       persons.push(person);
-       callback();
-   }, 2000); 
+function createPerson(person) {
+    let prom = new Promise(function (resolve, reject) {
+        persons.push(person);
+        let error = false;
+
+        if (!error) {
+            resolve();
+        }
+        else {
+            reject('Error!: Something Wrong!');
+        }
+
+    });
+    return prom;
 }
 
 function getPerson() {
-    setTimeout(function(){
+    setTimeout(function () {
         let output = '';
-        persons.forEach(function(person){
+        persons.forEach(function (person) {
             output += `<li>${person.firstName} ${person.lastName}</li>`
-        }); 
+        });
         document.getElementById('output').innerHTML = output;
     }, 500);
 }
 
-createPerson({firstName:"Rony", lastName: "Chy"}, getPerson);
-createPerson({firstName:"Jibon", lastName: "Ahmed"}, getPerson);
+createPerson({ firstName: "Rony", lastName: "Chy" })
+    .then(getPerson)
+    .catch(function (err) {
+        console.log(err);
+    });
