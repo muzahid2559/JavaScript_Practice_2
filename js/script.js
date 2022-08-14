c1bcc856e99c95fa4ccd9578eac771e0dc89ca91
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-14 19:59:11 +0600

    Global and Function Scope for (ES6 or ECMAScript6)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
-// Fetch API
-// Fetch API Uses JavaSript Promise
+//ES6 or ECMAScript6
 
-document.getElementById("get_data").addEventListener('click', getData);
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
 
-// xhr.open('GET', 'http://api.icndb.com/jokes/random/', true);
 
+console.log(`Global Scope: `, a, b, c);
 
-
-// Arrow Function
-
-function getData() {
-    fetch('http://api.icndb.com/jokes/random')
-        .then(res => res.json())
-        .then(data => { console.log(data); })
-        .catch(err => {console.log(err); })
+function test() {
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log(`Functions Scope: `, a, b ,c);
 }
+
+test();
+
+console.log(`Global Scope: `, a, b, c);
+
+if (true) {
+    var a = 7;
+    let b = 8;
+    const c = 9;
+    console.log(`If Scope: ` , a, b, c);
+}
+
+console.log(`Global Scope: `, a, b, c);
+
+
+for (let a = 0; a <10; a ++) {
+    console.log(`Loop: `, a);
+}
+
+
+console.log(`Global Scope: `, a, b, c);
