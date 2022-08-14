142efa7596475aecfb585379cdab458b8634deff
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-15 01:06:52 +0600

    Swapping

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-//Array Destructuring
-let fruits = ["Apple", "Grape", "Orange"];
+// Swap Variables
 
-let [fruit1,fruit2,fruit3] = fruits;
-console.log(fruit1,fruit2,fruit3);
 
-//Array Destructuring
-let fruit = ["Apple", "Grape", "Orange"];
+//normal swapping
+let a, b;
+a = 8; b= 20;
 
-let [fruits1, ,fruits3] = fruit;
-console.log(fruit1,fruit3);
+let temp = a;
+a = b;
+b = temp;
+
+console.log(`a = ${a} and b = ${b}`);
+
+
+//easily swapping
+let c, d;
+c = 10; d= 30;
+
+[c, d] = [d , c];
+
+console.log(`c = ${c} and d = ${d}`);
+
